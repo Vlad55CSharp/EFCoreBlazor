6b25564929c0e9cd4dad921dcde8f1a5b477d661
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-03-12 21:44:00 +0600

    Emaple 3 (Migrations)

---
+++ Example 3/EFCoreBlazorMigrations/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCoreBlazorMigrations.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int Age { get; set; }
+        public string? Role { get; set; } //новое свойство - роль пользователя в системе
+        public DateTime? CreatedAt { get; set; }
+    }
+}
